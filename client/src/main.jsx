362e328f45f09908d61f412f75c76c209cd68db5
jamie-mc21 <78818762+jamie-mc21@users.noreply.github.com>
2023-02-21 13:15:37 +0000

    authcontroller

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import { AuthContextProvider } from './context/AuthContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   <React.StrictMode>
+    <AuthContextProvider>
     <App />
+    </AuthContextProvider>
   </React.StrictMode>
   </BrowserRouter>
 );
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,6 +1,11 @@
+import { useContext } from 'react';
 import {Alert, Button, Form, Row, Col, Stack} from 'react-bootstrap';
+import { AuthContext } from '../context/AuthContext';
 
 const Register = () => {
+
+    const { registerInfo,updateRegisterInfo } = useContext(AuthContext);
+
     return ( 
     <>
     <Form>
@@ -12,9 +17,21 @@ const Register = () => {
             <Col xs={3}>
             <Stack gap={2}>
                 <h2>Register</h2>
-                <Form.Control type="text" placeholder="Username" />
-                <Form.Control type="email" placeholder="Email" />
-                <Form.Control type="password" placeholder="Password" />
+                <Form.Control type="text" placeholder="Username" 
+                onChange={(e) => 
+                    updateRegisterInfo({...registerInfo, name: e.target.value })
+                }
+                />
+                <Form.Control type="email" placeholder="Email"
+                onChange={(e) => 
+                    updateRegisterInfo({...registerInfo, email: e.target.value })
+                }
+                />
+                <Form.Control type="password" placeholder="Password" 
+                onChange={(e) => 
+                    updateRegisterInfo({...registerInfo, password: e.target.value })
+                }
+                />
                 <Button variant='primary' type='submit'>
                     Register
                 </Button>
