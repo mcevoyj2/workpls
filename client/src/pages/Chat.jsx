72756d2b86314f9b34de602cf425f337e5a5c60e
jamie-mc21 <78818762+jamie-mc21@users.noreply.github.com>
2023-02-22 16:39:07 +0000

    list chats and styling additions

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,16 +1,35 @@
 import { useContext } from "react";
+import { Container, Stack } from "react-bootstrap";
+import UserChat from "../components/chat/UserChat";
+import { AuthContext } from "../context/AuthContext";
 import { ChatContext } from "../context/chatContext";
 
 
 const Chat = () => {
+    const { user } = useContext(AuthContext);
 
-    const { userChats,
-        isUserChatsLoading,
-        userChatError,
+    const { userChats, isUserChatsLoading, userChatError,
     } = useContext(ChatContext);
 
-    console.log("userchats", userChats);
-    return (<>Chat</>);
+    return (
+    <Container>
+        {userChats?.length < 1 ? null : (
+        <Stack direction="horizontal" gap={4} className="align-items-start">
+            <Stack className="messages-box flex-grow-0 pe-3" gap={3}>
+            {isUserChatsLoading && <p>Loading chats...</p>}
+            {userChats?.map((chat,index) => {
+                return (
+                <div key={index}>
+                    <UserChat chat={chat} user={user}/>
+                </div>
+                );
+            })}
+            </Stack>
+            <p>ChatBox</p>
+            </Stack>
+            )}
+    </Container>
+    );
 };
 
 export default Chat;
