7afd5908f2d52f0f9a2032453568b6ca07b8aa5a
jamie-mc21 <78818762+jamie-mc21@users.noreply.github.com>
2023-02-28 18:53:12 +0000

    scroll

--- client/src/components/chat/ChatBox.jsx
+++ client/src/components/chat/ChatBox.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { Stack } from "react-bootstrap";
 import { AuthContext } from "../../context/AuthContext";
 import { ChatContext } from "../../context/chatContext";
@@ -11,9 +11,11 @@ const ChatBox = () => {
     const { currentChat, messages, isMessagesLoading, sendTextMessage } = useContext(ChatContext);
     const {recipientUser} = useFetchRecipientUser(currentChat, user);
     const [textMessage, setTextMessage] = useState("");
+    const scroll = useRef();
 
-    console.log("text", textMessage);
-
+    useEffect(() => {
+        scroll.current?.scrollIntoView({behavior: "smooth"});
+    }, [messages])
 
     if(!recipientUser) return (
         <p style={{textAlign: "center", width: "100%"}}> 
@@ -36,7 +38,9 @@ const ChatBox = () => {
             {messages && messages.map((message, index) => (
             <Stack key={index} className={`${message?.senderId === user?._id ? 
             "message self align-self-end flex-grow-0" : 
-            "message align-self-start flex-grow-0"}`}>
+            "message align-self-start flex-grow-0"}`}
+            ref= {scroll}
+            >
             <span>{message.text}</span>
             <span className="message-footer">
                 {moment(message.createdAt).calendar()}
